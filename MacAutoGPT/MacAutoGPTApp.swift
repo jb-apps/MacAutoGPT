77c3b6acdf270399111674022822f3341a59c017
Jonathan <maxjbv@gmail.com>
2023-04-16 22:07:20 +0200

    first commit

---
+++ MacAutoGPT/MacAutoGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacAutoGPTApp.swift
+//  MacAutoGPT
+//
+//  Created by Jonathan Benavides Vallejo on 16.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MacAutoGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
